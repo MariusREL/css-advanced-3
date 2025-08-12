bd1d445f21947ce025e84b7156ea961ef16ec6a2
Marius R <Mrel0425@gmail.com>
2025-08-12 20:31:31 +0200

    Add star animation and cloud elements to the main scene
    - Implemented `giveStarsClass` function for dynamic star classes.
    - Updated star image source to `Astrocat_04.svg`.
    - Enhanced header styling for better layout.
    - Added cloud elements to the main section with animation.
    - Introduced new CSS animations for star twinkling and cloud drifting.

--- app.js
+++ app.js
@@ -1,15 +1,25 @@
 const header = document.querySelector("#header");
 console.log(header);
 
+function giveStarsClass(num) {
+  if (num == 1 || num == 5 || num == 9) {
+    return "starBlink1";
+  } else if (num == 2 || num == 7 || num == 10) {
+    return "starBlink2";
+  } else {
+    return "starBlink3";
+  }
+}
+
 const numStars = 10; // Adjust number of stars
 let starsHTML = "";
 
 for (let i = 1; i <= numStars; i++) {
   starsHTML += `
         <img
-            src="./img/star-shine-svgrepo-com.svg"
+            src="./img/Astrocat_04.svg"
             alt="a shining star"
-            class="star star${i}"
+            class="star star${i} ${giveStarsClass(i)}"
            
         />
     `;
